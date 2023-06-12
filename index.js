43578d366a4b34e512f38dff5e7e544d8d1b0461
Karol Kaźmierczak <kazmierczakkarol1999@gmail.com>
2023-06-12 22:08:48 +0200

    machine

---
+++ index.js
@@ -0,0 +1,92 @@
+let current_step = 0;
+let current_move = 0;
+let steps = [];
+
+const nextButton = document.querySelector("#next-step");
+const resetButton = document.querySelector("#reset");
+const compileButton = document.querySelector("#compile");
+const textArea = document.querySelector("#input");
+const buttons = document.querySelectorAll(".element");
+const moveText = document.querySelector("#move");
+const actionNo = document.querySelector("#action-no");
+
+const reset = () => {
+    buttons.forEach((x) => (x.innerHTML = 0));
+    current_move = 0;
+    current_step = 0;
+};
+
+const resetValue = (n) => {
+    buttons[n].innerHTML = 0;
+};
+
+const addValue = (n) => {
+    let currentValue = parseInt(buttons[n].innerHTML);
+    buttons[n].innerHTML = ++currentValue;
+};
+
+const assignValue = (n, m) => {
+    buttons[m].innerHTML = buttons[n].innerHTML;
+};
+
+const move = (n, m, q) => {
+    if (buttons[m].innerHTML === buttons[n].innerHTML) {
+        current_step = q;
+    } else {
+        current_step++;
+    }
+};
+
+const actions = {
+    Z: resetValue,
+    S: addValue,
+    T: assignValue,
+    I: move,
+};
+
+compileButton.addEventListener("click", () => {
+    current_step = 0;
+
+    const text = textArea.value;
+
+    steps = text.split(";");
+
+    current_move = 0;
+    moveText.innerHTML = 0;
+    reset();
+});
+
+resetButton.addEventListener("click", reset);
+
+nextButton.addEventListener("click", () => {
+    moveText.innerHTML = ++current_move;
+
+    const step = steps[current_step];
+    const action = step.trim().charAt(0).toUpperCase();
+    if (action == "Z" || action == "S") {
+        arg = step.split("(")[1].charAt(0);
+        actions[action](arg);
+    }
+
+    if (action == "T") {
+        const arg1 = step.split("(")[1].split(",")[0].trim().charAt(0);
+        const arg2 = step.split("(")[1].split(",")[1].trim().charAt(0);
+
+        actions[action](arg1, arg2);
+    }
+
+    if (action == "I") {
+        console.log(step.split("(")[1].split(","));
+
+        const arg1 = step.split("(")[1].split(",")[0].trim().charAt(0);
+        const arg2 = step.split("(")[1].split(",")[1].trim().charAt(0);
+        const arg3 = step.split("(")[1].split(",")[2].trim().charAt(0);
+        actions[action](arg1, arg2, arg3);
+    } else {
+        current_step++;
+    }
+
+    actionNo.innerHTML = current_step;
+});
+
+reset();
---
+++ readme.md
@@ -0,0 +1,7 @@
+# Author Karol Kaźmierczak
+
+Aplikacja napisana w 30 min, po to, aby móc ćwiczyć maszynę licznikową na zajęcia
+
+# ENG
+
+The application was written in 30 minutes to be able to use the counter machine for classes
